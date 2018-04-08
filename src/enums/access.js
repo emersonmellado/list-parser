5ec14e3f4b488b823ca8a62d3f004986bbbb7916
Emerson <emersonmellado@gmail.com>
2018-04-08 11:52:31 -0700

    Creating access enum

---
+++ src/enums/access.js
@@ -0,0 +1,10 @@
+const Access = {
+    Private: 1,
+    Public: 2,
+    properties: {
+        0: { key: 0, text: "Private", value: 0, icon: "private", color: "grey" },
+        1: { key: 1, text: "Public", value: 1, icon: "public", color: "green" }
+    }
+};
+
+module.exports = Access;
